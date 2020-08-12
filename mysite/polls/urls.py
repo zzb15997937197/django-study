533df25ea9a52197e0e7cf0bac3635fe9e30e434
zhangzhengbing <zhangzhengbing@huayin.com>
2020-08-12 15:22:11 +0800

    1.通过templates来创建html页面

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -7,12 +7,11 @@ urlpatterns = [
     # ex: polls
     # name属性可以随意取
     path('', views.index, name='index'),
-    path('<int:q_id>/', views.detail, name='detail'),
-    path('<int:q_id>/result', views.result, name='result'),
     path('insert/record/<name>', InsertUser.as_view()),
     path('get/records', SelectUser.as_view()),
     path('login', UserLogin.as_view()),
     path('logout', UserLogout.as_view()),
-    path('get/class/info/<uid>', MyClassInfo.as_view())
+    path('get/class/info/<uid>', MyClassInfo.as_view()),
+    path('hello',views.hello,name='hello')
 
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.返回给前端页面
-
+import datetime
+from django.template.loader import get_template
+from django.template.context import Context
 from django.http import HttpResponse
+
+from django.shortcuts import render, render_to_response, redirect
+
+
 def index(request):
     return HttpResponse("hello word,you are at the poll index")
+
+
+def hello(request):
+    return render(request, "hello.html")
